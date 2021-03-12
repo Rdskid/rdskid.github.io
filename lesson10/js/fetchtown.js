a4009de44017b13e419632f811fb5bd5d60fa745
rdskid <dskidmorelds@gmail.com>
2021-03-12 15:04:56 -0700

    final push week 10

---
+++ lesson10/js/fetchtown.js
@@ -0,0 +1,41 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < towns.length; i++ ) {
+        if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+        let card = document.createElement('section');
+        let info = document.createElement('div');
+        let h2 = document.createElement('h2');
+        let motto = document.createElement('p');
+        let yearFounded = document.createElement('p');
+        let currentPopulation = document.createElement('p');
+        let averageRainfall = document.createElement('p');
+        let image = document.createElement('img');
+
+        h2.textContent = towns[i].name
+        motto.textContent = ' ' + towns[i].motto;
+        yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
+        averageRainfall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall +'in.';
+
+        info.appendChild(h2);
+        info.appendChild(motto);
+        info.appendChild(yearFounded);
+        info.appendChild(currentPopulation);
+        info.appendChild(averageRainfall);
+
+        card.appendChild(info);
+        card.appendChild(image);
+
+        document.querySelector('div.cards').appendChild(card);
+        image.setAttribute('src', 'images/' + towns[i].photo);
+        image.setAttribute('alt', towns[i].name + ' with photo');
+        }
+    }
+  });
